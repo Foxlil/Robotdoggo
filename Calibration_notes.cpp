1a1e66323b0513b3fe62bce1582f5716d867e40f
Florence <Flou119@easternms.org>
2026-06-20 22:17:54 -0400

    Calibrated the shoulder servo, linkage servo, and hip servo of the robot dog's bottom left leg. Previously 0-180 degrees as instructed by code did not actually output desired 0-180 degrees----tested values until servos were able to rotate with satisfactory range

---
+++ Calibration_notes.cpp
@@ -0,0 +1,31 @@
+/* Notes for servo calibration */
+
+/* BOTTOM LEFT LEG: 
+12/ Shoulder servo: 
+Minimum position: 130 
+Maximum position: 630 
+
+13/ Linkage servo: 
+Minimum position: 110 
+Maximum position: 600 
+
+14/ Hip servo: 
+Minimum position: 130
+Maximum position: 630
+*/
+
+/* BOTTOM RIGHT LEG:
+
+
+
+*/
+
+/* TOP LEFT LEG: 
+5/ Linkage servo: 
+Minumum position: 120
+Maximum position: 620
+
+6/ Hip servo:
+Minumum position: 150
+Maximum position: 650
+*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,21 +1,22 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <Arduino.h>
+
 Adafruit_PWMServoDriver servo = Adafruit_PWMServoDriver();
 
 #define servoMIN 150
-#define servoMAX 600
+#define servoMAX 650
 
 void setup() {
   servo.begin();
   servo.setPWMFreq(60);
-  Serial.begin(9600);
 }
 
 void loop() {
-    Serial.println("Starting");
-    servo.setPWM(12, 0, servoMIN);
+    servo.setPWM(5, 0, 120);
+    servo.setPWM(6, 0, servoMIN);
     delay(1000);
-    servo.setPWM(12, 0, servoMAX);
+    servo.setPWM(5, 0, 620);
+    servo.setPWM(6, 0, servoMAX);
     delay(2000);
 }
